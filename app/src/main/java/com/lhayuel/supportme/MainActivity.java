c49ab0c269c068fb5ef5e621195960c217e78110
Govinda Ghimeray <ghimry@gmail.com>
2020-11-30 01:09:24 +0600

    Registration Code added

--- app/src/main/java/com/lhayuel/supportme/MainActivity.java
+++ app/src/main/java/com/lhayuel/supportme/MainActivity.java
@@ -2,13 +2,17 @@ package com.lhayuel.supportme;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+//implementation 'com.android.support:design:26.+*' for appbar changing
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Intent intent = new Intent(MainActivity.this, RegistrationActivity.class);
+        startActivity(intent);
     }
 }
